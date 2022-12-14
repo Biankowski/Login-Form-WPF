d6292c9bc202cf4d2171f3ca3fa4d1e04408e111
Biankowski <rodrigobianchini96@gmail.com>
2022-12-14 18:39:42 -0300

    First commit

---
+++ Preparo/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Preparo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Pessoa> pessoa = new List<Pessoa>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            pessoa.Add(new Pessoa { FirstName = "Rodrigo", LastName = "Bianchini"});
+            pessoa.Add(new Pessoa { FirstName = "Ana", LastName = "Gantois" });
+
+
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Successfully Logged In as {userNameText.Text}");
+        }
+
+        private void registerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Successfully Registered");
+        }
+    }
+}
---
+++ Preparo/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Preparo
+{
+    public class Pessoa
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
